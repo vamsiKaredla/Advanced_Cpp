4970573144d57521a37a70063d6320d92eed679a
vamsiKaredla <57910564+vamsiKaredla@users.noreply.github.com>
2022-12-06 11:06:18 +0530

    Function Pointers

--- Function_Pointers/functionPointers.cpp
+++ Function_Pointers/functionPointers.cpp
@@ -4,6 +4,7 @@
 #include<string>
 using namespace std;
 
+// function to check the size of string
 bool match(string text) {
 	if(text.size()==3) {
 	 return true;
@@ -11,6 +12,7 @@ bool match(string text) {
 	return false;
 }
 
+// example of function which takes function pointer as a parameter
 int count(const vector<string> strings,bool (*match)(string text)) {
 	int count =0;
 	for(int i=0; i<strings.size();i++) {
@@ -28,7 +30,7 @@ int main() {
 	texts.push_back("three");
 	texts.push_back("four");
 	texts.push_back("five");
-
+	//passing function as a parameter
 	cout<<count(texts,match);
 
 	return 0;
